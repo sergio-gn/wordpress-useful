6d79e0f04fcd4316563a0aaad7b5966f0818db05
Sergio Garcia Neto <88346250+sergio-gn@users.noreply.github.com>
2023-05-23 14:46:49 +1000

    menu simple

---
+++ menu-nav/menu.php
@@ -0,0 +1,15 @@
+<nav class="site-navigation">
+    <ul class="menu">
+        <li><a href="#">Home</a></li>
+        <li class="menu-item-has-children">
+            <a href="#">Services</a>
+            <ul class="sub-menu">
+                <li><a href="#">Service 1</a></li>
+                <li><a href="#">Service 2</a></li>
+                <li><a href="#">Service 3</a></li>
+            </ul>
+        </li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+</nav>
