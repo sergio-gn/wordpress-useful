96a1624e958064a2d006f040804444e6b933eb74
Sergio Garcia Neto <88346250+sergio-gn@users.noreply.github.com>
2023-06-15 10:07:22 +1000

    Update admin-bar-hide-or-show.php

--- admin-bar-hide-or-show.php
+++ admin-bar-hide-or-show.php
@@ -7,10 +7,11 @@ add_filter( 'show_admin_bar', '__return_false' );
 /**
  * Show admin bar
  */
-function show_admin_bar_on_pages( $show ) {
-    if ( is_page() ) {
-        return true;
+function show_admin_bar_for_logged_in_users() {
+    if (is_user_logged_in()) {
+        add_filter('show_admin_bar', '__return_true');
+    } else {
+        add_filter('show_admin_bar', '__return_false');
     }
-    return $show;
 }
-add_filter( 'show_admin_bar', 'show_admin_bar_on_pages' );
+add_action('after_setup_theme', 'show_admin_bar_for_logged_in_users');
