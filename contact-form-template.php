373732e16005ec466a0d3268cb48a0cbe89ec259
Sergio Garcia Neto <88346250+sergio-gn@users.noreply.github.com>
2023-06-29 16:01:10 +1000

    Create contact-form-template.php

---
+++ contact-form-template.php
@@ -0,0 +1,21 @@
+<table border="10" cellpadding="10" style="border-collapse:collapse">
+    <tr>
+        <td style="background-color:cornsilk">From:</td>
+        <td>[txtEmail]</td>
+    </tr>
+    <tr>
+        <td style="background-color:cornsilk">Name:</td>
+        <td>[txtName]</td>
+    </tr>
+    <tr>
+        <td style="background-color:cornsilk">Phone:</td>
+        <td>[txtMobile]</td>
+    </tr>
+    <tr>
+        <td style="background-color:cornsilk">Job Location:</td>
+        <td>[txtAddress]</td>
+    </tr>
+</table>
+<br>
+--
+This e-mail was sent from a contact form on Example and Cooling (example.com)
