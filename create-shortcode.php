9f6b7233068748e9d2f2b18613c57136d958bdca
Sergio Garcia Neto <88346250+sergio-gn@users.noreply.github.com>
2023-11-21 08:49:42 +1100

    Create create-shortcode.php

---
+++ create-shortcode.php
@@ -0,0 +1,9 @@
+<?php
+function custom_shortcode(){
+	return '<div id="brands-container">
+	<div class="brand">
+	  do stuff here
+   </div>
+}'
+add_shortcode('useThisOnWordPress', 'custom_shortcode');
+?>
